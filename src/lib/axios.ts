c0fcf1ca13aa0b55e1efe7c84530ddfaa625a5fb
bagasriatmaputera <bagasriatmap@gmail.com>
2026-01-13 21:45:57 +0700

    update: baseURL for local

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost/sipesda/public/api", // Sesuaikan dengan URL Laravel Anda
+    baseURL: "http://127.0.0.1:8000/sipesda/public/api", // Sesuaikan dengan URL Laravel Anda
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
