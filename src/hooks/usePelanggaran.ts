99515b0c3511331c55989bb0d64c4e0c188dbd9c
bagasriatmaputera <bagasriatmap@gmail.com>
2026-01-18 20:51:43 +0700

    feat: add hooks pelanggaran

--- src/hooks/usePelanggaran.ts
+++ src/hooks/usePelanggaran.ts
@@ -1,37 +1,53 @@
-import axiosInstance from "@/lib/axios"
-import type { Pelanggaran } from "@/types/type"
-import { useEffect, useState } from "react"
+import axiosInstance from "@/lib/axios";
+import type { Pelanggaran } from "@/types/type";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export const usePelanggaran = () => {
-    const [pelanggaran, setPelanggaran] = useState<Pelanggaran[]>([]);
-    const [loading, setLoading] = useState(false);
+  const [pelanggaran, setPelanggaran] = useState<Pelanggaran[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    const fetchPelanggaran = async () => {
-        setLoading(true);
-        try {
-            const res = await axiosInstance.get("/pelanggaran");
-            setPelanggaran(res.data.data);
-        } catch (err) {
-            console.error("Gagal ambil data pelanggaran siswa", err);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const deletePelanggaran = async (id: number) => {
-        try {
-            await axiosInstance.delete(`/pelanggaran/delete/${id}`);
-            fetchPelanggaran();
-        } catch (err) {
-            throw err
-        }
-
+  const fetchPelanggaran = async () => {
+    setLoading(true);
+    try {
+      const res = await axiosInstance.get("/pelanggaran");
+      setPelanggaran(res.data.data);
+    } catch (err) {
+      console.error("Gagal ambil data pelanggaran siswa", err);
+    } finally {
+      setLoading(false);
     }
+  };
 
+  const storePelanggaran = async (any: any) => {
+    try {
+      await axiosInstance.post("/pelanggaran/create-pelanggaran", any);
+      fetchPelanggaran();
+      toast.success("Berhasil input pelanggaran siswa!");
+    } catch (error) {
+      toast.error("Gagal input pelanggaran, mohon coba lagi");
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchPelanggaran();
-    }, [])
+  const deletePelanggaran = async (id: number) => {
+    try {
+      await axiosInstance.delete(`/pelanggaran/delete/${id}`);
+      fetchPelanggaran();
+    } catch (err) {
+      throw err;
+    }
+  };
 
-    return { pelanggaran, loading,refresh: fetchPelanggaran, deletePelanggaran }
-}
+  useEffect(() => {
+    fetchPelanggaran();
+  }, []);
+
+  return {
+    pelanggaran,
+    storePelanggaran,
+    loading,
+    refresh: fetchPelanggaran,
+    deletePelanggaran,
+  };
+};
